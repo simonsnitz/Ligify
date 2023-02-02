c3f7422acc92f1c7fb2444f8a9fc395231eb0df4
Simon d'Oelsnitz <simonsnitz@gmail.com>
2023-02-02 08:28:39 -0500

    added MVP for BLASTing GroovDB

--- calc_similarity/chemical/tanimoto.py
+++ calc_similarity/chemical/tanimoto.py
@@ -2,6 +2,7 @@ from rdkit import Chem
 from rdkit.Chem import AllChem
 from rdkit import DataStructs
 import json
+import requests
 
 
 def tanimoto_calc(smi1, smi2):
@@ -12,21 +13,38 @@ def tanimoto_calc(smi1, smi2):
     s = round(DataStructs.TanimotoSimilarity(fp1,fp2),3)
     return s
 
-    # isoprene
-target = "CC(=C)C=C"
 
-with open("groovLigands.json", "r") as f:
-    data = json.load(f)
+def compare_chemicals(target: str):
+        # isoprene
+    # target = "CC(=C)C=C"
+    with open("groovLigands.json", "r") as f:
+        data = json.load(f)
 
-scores = []
-for i in data:
-    tani = tanimoto_calc(i["smiles"], target)
-    scores.append({"name": i["name"], "score": tani})
+    def get_score(chem):
+        return chem.get('score')
 
-def get_score(chem):
-    return chem.get('score')
+    scores = []
+    for i in data:
+        tani = tanimoto_calc(i["smiles"], target)
+        scores.append({"name": i["name"], "score": tani})
+    
+    scores.sort(key=get_score, reverse=True)
+    print(scores)
 
-scores.sort(key=get_score, reverse=True)
-print(scores)
 
-# print(tanimoto_calc(berberine, thp))
+
+    # Fetch data from GroovDB via an AWS lambda function.
+        # I'll need to create a new lambda function to get the data we need.
+def get_smiles():
+    
+    response = requests.get('https://4lsuwlkqoe.execute-api.us-east-2.amazonaws.com/search',
+        headers={ 'Accept': 'application/json', 'Authorization': 'groovDBAllowPostToDB_%FYaeF36!8'})
+
+    data = response.text
+    print(data)
+
+
+
+
+data = get_smiles()
+print(data)
