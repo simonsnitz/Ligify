6537d503e69e7c83fb3d3dddec34a80f284c21e3
Simon d'Oelsnitz <simonsnitz@gmail.com>
2023-05-22 14:20:17 -0400

    called streamlit_run() directly in ligify.py

--- ligify.py
+++ ligify.py
@@ -9,5 +9,7 @@ def streamlit_run():
     chem, regulator_column, data_column, prog, chemical_name, filters = run_streamlit()
     run_ligify(chem, regulator_column, data_column, prog, chemical_name, filters)
 
-if __name__ == '__main__':
-    streamlit_run()
+streamlit_run()
+
+# if __name__ == '__main__':
+#     streamlit_run()
