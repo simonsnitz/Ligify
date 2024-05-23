5b0af2f03a0efbda90ce43130c8e8d3163a8a616
Josh Love <joshdavidlove@gmail.com>
2024-05-23 18:20:33 -0500

    Working job queue

--- streamlit/ligify/format_results.py
+++ streamlit/ligify/format_results.py
@@ -62,198 +62,161 @@ def download_df(ligand_name, genbank_con):
 
 
 
-def format_results(data_column, ligand_name):
+def format_results(data_column, ligand_name, data):
+    
+    print(len(data))
+    
+    print(data)
 
-    if st.session_state.data:
+    refseq = data['refseq']
+    data_column.write("")
+    data_column.write("")
+    data_column.markdown(f'<h1 style="text-align: center; color: black; margin-top: -50px;">{refseq}</h1>', unsafe_allow_html=True)
 
-        # Header
-        refseq = st.session_state.data['refseq']
-        data_column.write("")
-        data_column.write("")
-        data_column.markdown(f'<h1 style="text-align: center; color: black; margin-top: -50px;">{refseq}</h1>', unsafe_allow_html=True)
+    data_column.divider()
 
-        data_column.divider()
+    # Spacer
+    data_column.text("")
+    data_column.text("")
 
+    # Regulator info
+    reg_ncbi_anno = data["annotation"]
+    reg_protein_seq = data["reg_protein_seq"]
+    reg_uniprot_id = data["uniprot_reg_data"]["id"]
+    organism = data["protein"]["organism"]
+    organism_name = str(organism[-2]) + ", " + str(organism[-1])
 
-        # Spacer
-        data_column.text("")
-        data_column.text("")
+    reg_json = {
+        "name": "Regulator attribute",
+        "NCBI annotation": reg_ncbi_anno,
+        "Uniprot ID": reg_uniprot_id,
+        "Protein length": len(reg_protein_seq),
+        "Organism": organism_name,
+    }
+    
+    # Regulator container
+    regulator_con = data_column.container()
 
+    # GenBank container
+    genbank_con = data_column.container()   
 
-        # Regulator info
-        reg_ncbi_anno = st.session_state.data["annotation"]
-        reg_protein_seq = st.session_state.data["reg_protein_seq"]
-        reg_uniprot_id = st.session_state.data["uniprot_reg_data"]["id"]
-        organism = st.session_state.data["protein"]["organism"]
-        organism_name = str(organism[-2])+", "+str(organism[-1])
+    reg_info, reg_spacer, reg_genbank = regulator_con.columns((8,1,5))
+    regulator_df = pd.DataFrame(reg_json, index=["name"]).astype(str)
+    regulator_df.set_index("name", inplace=True)
+    regulator_df = regulator_df.T
+    reg_info.subheader("Regulator information")
+    reg_info.table(regulator_df)
 
-        reg_json = {
-            "name": "Regulator attribute",
-            "NCBI annotation": reg_ncbi_anno,
-            "Uniprot ID": reg_uniprot_id,
-            "Protein length": len(reg_protein_seq),
-            "Organism": organism_name,
-        }
-        
-            #Regulator container
-        regulator_con = data_column.container()
+    reg_genbank.header("")
+    reg_genbank.header("")
+    reg_genbank.form_submit_button(label=f"Show Plasmid for {data['refseq']}", type="primary", on_click=download_df, args=(ligand_name,genbank_con))
+    reg_genbank.markdown(f'<p style="font-size: 16px">This plasmid is designed to induce GFP expression in the presence of the target molecule via {refseq}, within E. coli</>', unsafe_allow_html=True)
 
-            #GenBank container
-        genbank_con = data_column.container()   
+    # Enzyme info
+    enz_annotation = data['protein']['enzyme']['description']
+    enz_refseq = data['protein']['enzyme']['ncbi_id']
+    equation = data['equation'] 
+    rhea_id = data['rhea_id'] 
+    references = data['protein']['enzyme']['dois']
 
-        reg_info, reg_spacer, reg_genbank = regulator_con.columns((8,1,5))
-        regulator_df = pd.DataFrame(reg_json, index=["name"]).astype(str)
-        regulator_df.set_index("name", inplace=True)
-        regulator_df = regulator_df.T
-        reg_info.subheader("Regulator information")
-        reg_info.table(regulator_df)
+    enz_json = {
+        "name": "Enzyme attribute",
+        "Annotation": enz_annotation,
+        "Reaction": equation,
+        "RHEA ID": rhea_id,
+        "RefSeq ID": enz_refseq,
+    }
 
-        reg_genbank.header("")
-        reg_genbank.header("")
-        reg_genbank.form_submit_button(label="Show Plasmid", type="primary", on_click=download_df, args=(ligand_name,genbank_con))
-        reg_genbank.markdown(f'<p style="font-size: 16px">This plasmid is designed to induce GFP expression in the presence of the target molecule via '+str(refseq)+', within E. coli</>', unsafe_allow_html=True)
+    enzyme_and_lig = data_column.container()
+    enz, alt_lig = enzyme_and_lig.columns([1,1])
+    enz_df = pd.DataFrame(enz_json, index=[0])
+    enz_df.set_index("name", inplace=True)
+    enz_df = enz_df.T
+    enz.subheader("Associated enzyme")
+    enz.table(enz_df)
 
+    # Alternative ligands
+    alt_lig.subheader("Possible alternative ligands")
+    alt_lig_list = data['alt_ligands'] 
+    lig_html = "<ul style='height: 180px; overflow-y: scroll'>"
+    for lig in alt_lig_list:
+        lig_html += "<li>" + lig + "</li>"
+    lig_html += "</ul>"
+    alt_lig.markdown(lig_html, unsafe_allow_html=True)
 
+    # Enzyme references
+    ref_and_rank = data_column.container()
+    reference_col, rank_col = ref_and_rank.columns(2)
+    reference_col.subheader("Enzyme references")
+    for i in references:
+        reference_col.markdown(f'<a target="__blank">{"https://doi.org/"+i}</a>', unsafe_allow_html=True)
 
+    # Rank metrics
+    rank_col.subheader("Rank description")
+    rank_metrics = data["rank"]["metrics"]
 
-        # Enzyme info
-        enz_annotation = st.session_state.data['protein']['enzyme']['description']
-        enz_refseq = st.session_state.data['protein']['enzyme']['ncbi_id']
-        equation = st.session_state.data['equation'] 
-        rhea_id = st.session_state.data['rhea_id'] 
-        references = st.session_state.data['protein']['enzyme']['dois']
+    rank_df = pd.DataFrame(rank_metrics).T
+    rank_col.dataframe(rank_df)
 
-        enz_json = {"name": "Enzyme attribute",
-                    "Annotation": enz_annotation,
-                    "Reaction": equation,
-                    "RHEA ID": rhea_id,
-                    "RefSeq ID": enz_refseq,
-                    }
+    # Operon
+    operon_seq = data_column.container()
+    operon_seq.subheader("Operon")
+    operon_data = data['protein']['context']
+    operon_json = operon_data['operon']
+    
+    with operon_seq.expander(label=f"Click here to see the genetic context for {refseq}"):
+        # Get the regulator position within the operon
+        reg_index = 0
+        for i in operon_json:
+            if i["accession"] == refseq:
+                break
+            else:
+                reg_index += 1
 
-        enzyme_and_lig = data_column.container()
-        enz, alt_lig = enzyme_and_lig.columns([1,1])
-        enz_df = pd.DataFrame(enz_json, index=[0])
-        enz_df.set_index("name", inplace=True)
-        enz_df = enz_df.T
-        enz.subheader("Associated enzyme")
-        enz.table(enz_df)
+        # Get the enzyme position within the operon
+        enz_index = data['protein']['context']['enzyme_index']
 
+        # Create the operon table
+        genes = []
+        for i in operon_json:
+            gene = {
+                "RefSeq ID": i['accession'],
+                "Description": i.get("description", " "),
+                "Direction": i['direction'],
+                "Start position": i['start'],
+                "End position": i['stop']
+            }
+            genes.append(gene)
+        operon_df = pd.DataFrame(genes)
 
-        # Alternative ligands
-        alt_lig.subheader("Possible alternative ligands")
-        alt_lig_list = st.session_state.data['alt_ligands'] 
-        lig_html = "<ul style='height: 180px; overflow-y: scroll'>"
-        for lig in alt_lig_list:
-            lig_html += "<li>" + lig + "</li>"
-        lig_html += "</ul>"
-        alt_lig.markdown(lig_html, unsafe_allow_html=True)
-            #alt_lig.markdown("- " + lig + "\n")
-        #alt_lig.table(st.session_state.data['alt_ligands'])
-            #TODO: Clean this table up
+        # Color the operon table
+        colors = ["#fcb1b1", "#e6cffc", "#9afcac", "#fcc99d", "#a3fff6", "#fdff9c", "#ccd4fc", "#fcbdf6"]
+        def bg_color_col(col):
+            color = [colors[i % len(colors)] for i, x in col.items()]
+            return ['background-color: %s' % color[i] if col.name == 'RefSeq ID' or (col.name == "Description" and i == reg_index) or (col.name == "Description" and i == enz_index) else '' for i, x in col.items()]
 
+        operon_df = operon_df.style.apply(bg_color_col)
+        st.table(operon_df)
 
-        # Enzyme references
-        ref_and_rank = data_column.container()
-        reference_col, rank_col = ref_and_rank.columns(2)
-        reference_col.subheader("Enzyme references")
-        for i in references:
-            reference_col.markdown(f'<a target="__blank">{"https://doi.org/"+i}</a>', unsafe_allow_html=True)
-        
+        # Display the predicted promoter
+        st.markdown("<h5>Predicted promoter</h5>", unsafe_allow_html=True)
+        st.write(operon_data["promoter"]['regulated_seq'])
 
-
-        # Rank metrics
-        rank_col.subheader("Rank description")
-        rank_metrics = st.session_state.data["rank"]["metrics"]
-
-        rank_df = pd.DataFrame(rank_metrics).T
-        #orient='index')
-        # rank_df = pd.DataFrame.from_dict({(i,j): rank_metrics[i][j] 
-        #                    for i in rank_metrics.keys() 
-        #                    for j in rank_metrics[i].keys()},
-        #                orient='index')
-        rank_col.dataframe(rank_df)
-
-
-
-
-
-        # Operon
-        operon_seq = data_column.container()
-        operon_seq.subheader("Operon")
-        operon_data = st.session_state.data['protein']['context']
-        operon_json = operon_data['operon']
-        
-        with operon_seq.expander(label="Click here to see the genetic context for "+str(refseq)):
-
-
-            # Get the regulator position within the operon
-            reg_index = 0
-            for i in operon_json:
-                if i["accession"] == refseq:
-                    break
-                else:
-                    reg_index += 1
-
-            # Get the enzyme position within the operon
-            enz_index = st.session_state.data['protein']['context']['enzyme_index']
-
-
-            # Create the operon table
-            genes = []
-            for i in operon_json:
-                if "description" in i.keys():
-                    gene = {
-                        "RefSeq ID": i['accession'],
-                        "Description": i['description'],
-                        "Direction": i['direction'],
-                        "Start position": i['start'],
-                        "End position": i['stop']
-                    }
-                else:
-                    gene = {
-                        "RefSeq ID": i['accession'],
-                        "Description": " ",
-                        "Direction": i['direction'],
-                        "Start position": i['start'],
-                        "End position": i['stop']
-                    }
-                genes.append(gene)
-            operon_df = pd.DataFrame(genes)
-
-            # Color the operon table
-            colors = ["#fcb1b1", "#e6cffc", "#9afcac", "#fcc99d", "#a3fff6", "#fdff9c", "#ccd4fc", "#fcbdf6"]
-            def bg_color_col(col):
-                color = [colors[i % len(colors)] for i,x in col.items()]
-                return ['background-color: %s' % color[i]
-                            if col.name=='RefSeq ID' or (col.name=="Description" and i==reg_index) or (col.name=="Description" and i==enz_index)
-                            else ''
-                        for i,x in col.items()]
-
-            operon_df = operon_df.style.apply(bg_color_col)
-            st.table(operon_df)
-
-
-            # Display the predicted promoter
-            st.markdown("<h5>Predicted promoter</h5>", unsafe_allow_html=True)
-            st.write(operon_data["promoter"]['regulated_seq'])
-
-
-            # Create and display the color-annotated genome fragment
-            operon_seq = ""
-
-            c = 0
-            for seq in operon_data["operon_seq"]:
-                sequence = operon_data["operon_seq"][seq]
-                if re.compile(r"spacer").search(seq):
-                    html = "<span style='color: grey;'>"+str(sequence)+"</span>"
-                elif re.compile(r"overlap").search(seq):
-                    html = "<span style='color: red;'>"+str(sequence)+"</span>"
-                elif re.compile(r"fwd").search(seq):
-                    html = f"<b><span style='background: {colors[c % len(colors)]};'>"+str(sequence)+"</span></b>"
-                    c += 1
-                else:
-                    html = f"<span style='background: {colors[c % len(colors)]};'>"+str(sequence)+"</span>"
-                    c += 1
-                operon_seq += html
-            st.markdown("<h5>Full operon sequence</h5>", unsafe_allow_html=True)
-            st.markdown(operon_seq, unsafe_allow_html=True)     
+        # Create and display the color-annotated genome fragment
+        operon_seq_html = ""
+        c = 0
+        for seq in operon_data["operon_seq"]:
+            sequence = operon_data["operon_seq"][seq]
+            if re.compile(r"spacer").search(seq):
+                html = "<span style='color: grey;'>" + str(sequence) + "</span>"
+            elif re.compile(r"overlap").search(seq):
+                html = "<span style='color: red;'>" + str(sequence) + "</span>"
+            elif re.compile(r"fwd").search(seq):
+                html = f"<b><span style='background: {colors[c % len(colors)]};'>" + str(sequence) + "</span></b>"
+                c += 1
+            else:
+                html = f"<span style='background: {colors[c % len(colors)]};'>" + str(sequence) + "</span>"
+                c += 1
+            operon_seq_html += html
+        st.markdown("<h5>Full operon sequence</h5>", unsafe_allow_html=True)
+        st.markdown(operon_seq_html, unsafe_allow_html=True)
--- streamlit/ligify/streamlit_app.py
+++ streamlit/ligify/streamlit_app.py
@@ -221,66 +221,67 @@ def run_streamlit():
 
 # This is essentially the frontend component of the Ligify Web applications.
 
-def run_ligify(chem, results, progress, chemical, filters):
+def run_ligify(chem, results, progress, chemical, filters, regulators, metrics):
 
     m_spacer1, metrics_col, m_spacer2 = results.container().columns((1,3,1))
     regulator_column, data_column = results.columns([1,3])
+    
+    print('inside')
 
-    if st.session_state.SUBMITTED:
-
-        if chemical["smiles"] == None:
+    if chemical["smiles"] == None:
             data_column.subheader("Chemical input was not recognized. Please try a different input method.")
 
-        else:
+    else:
 
-            # SMILES = str(chemical_smiles)
-            # chem.image(f'http://hulab.rxnfinder.org/smi2img/{SMILES}/', width=200)
-
+        # SMILES = str(chemical_smiles)
+        # chem.image(f'http://hulab.rxnfinder.org/smi2img/{SMILES}/', width=200)
+        
+        if not regulators and not metrics:
             regulators, metrics = fetch_data(chemical["InChiKey"], filters)
 
-            select_spacerL, please_select, select_spacerR  = data_column.container().columns([1,2,1])     
+        select_spacerL, please_select, select_spacerR  = data_column.container().columns([1,2,1])     
+        
+        for entry in regulators:
+            format_results(data_column, chemical["name"], entry)
 
-            format_results(data_column, chemical["name"])
+        regulator_column.header('')
+        regulator_column.subheader('Sensor candidates')
+        regulator_column.divider()
 
-            regulator_column.header('')
-            regulator_column.subheader('Sensor candidates')
-            regulator_column.divider()
+        # If no regulators are returned, suggest alternative queries
+        if regulators == None:
+            similar_chemicals = blast_chemical(chemical["smiles"], filters["max_alt_chems"])
+            regulator_column.write("No regulators found")
+            please_select.subheader("No associated reactions   :pensive:") 
+            please_select.write("Consider an alternative query")   
+            data_column.dataframe(similar_chemicals)
+            
+        # If regulators are returned, format display
+        else:
 
-            # If no regulators are returned, suggest alternative queries
-            if regulators == None:
-                similar_chemicals = blast_chemical(chemical["smiles"], filters["max_alt_chems"])
-                regulator_column.write("No regulators found")
-                please_select.subheader("No associated reactions   :pensive:") 
-                please_select.write("Consider an alternative query")   
-                data_column.dataframe(similar_chemicals)
-                
-            # If regulators are returned, format display
-            else:
+            # Metrics data
+            metrics_col.subheader("Search metrics")
+            m_rhea, m_genes, m_filtered, m_operons, m_regs = metrics_col.columns(5)
+            m_rhea.metric("Rhea reactions", metrics["RHEA Reactions"])
+            m_genes.metric("Bacterial genes", metrics["Total genes"])
+            m_filtered.metric("Filtered genes", metrics["Filtered genes"])
+            m_operons.metric("Operons", metrics["Total operons"])
+            m_regs.metric("Regulators", metrics["Total regulators"])
+            metrics_col.divider()
 
-                # Metrics data
-                metrics_col.subheader("Search metrics")
-                m_rhea, m_genes, m_filtered, m_operons, m_regs = metrics_col.columns(5)
-                m_rhea.metric("Rhea reactions", metrics["RHEA Reactions"])
-                m_genes.metric("Bacterial genes", metrics["Total genes"])
-                m_filtered.metric("Filtered genes", metrics["Filtered genes"])
-                m_operons.metric("Operons", metrics["Total operons"])
-                m_regs.metric("Regulators", metrics["Total regulators"])
-                metrics_col.divider()
+            if not st.session_state.data:
+                please_select.subheader("Please select a regulator") 
 
-                if not st.session_state.data:
-                    please_select.subheader("Please select a regulator") 
+            reg_acc_col, rank_col = regulator_column.columns((2,1))
+            reg_acc_col.markdown("<h5>Regulator</h5>", unsafe_allow_html=True)
+            rank_col.markdown("<h5>Rank</h5>", unsafe_allow_html=True)
 
-                reg_acc_col, rank_col = regulator_column.columns((2,1))
-                reg_acc_col.markdown("<h5>Regulator</h5>", unsafe_allow_html=True)
-                rank_col.markdown("<h5>Rank</h5>", unsafe_allow_html=True)
-
-                for i in range(0, len(regulators)):
-                    name = "var"+str(i)
-                    rank = regulators[i]["rank"]["rank"]
-                    color = regulators[i]["rank"]["color"]
-                    rank_col.markdown(f"<p style='font-size:20px; font-weight: 600; color: {color};'>{rank}</p>", unsafe_allow_html=True)
-                    name = reg_acc_col.form_submit_button(regulators[i]['refseq'])
-                    if name:
-                        st.session_state.data = regulators[i]
-                        st.experimental_rerun()
-
+            for i in range(0, len(regulators)):
+                name = "var"+str(i)
+                rank = regulators[i]["rank"]["rank"]
+                color = regulators[i]["rank"]["color"]
+                rank_col.markdown(f"<p style='font-size:20px; font-weight: 600; color: {color};'>{rank}</p>", unsafe_allow_html=True)
+                name = reg_acc_col.form_submit_button(regulators[i]['refseq'])
+                if name:
+                    st.session_state.data = regulators[i]
+                    st.experimental_rerun()
